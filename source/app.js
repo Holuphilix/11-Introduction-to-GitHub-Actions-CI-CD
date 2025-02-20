425cb419a9e5a3455bfd05faa3ffed3e3b3e1744
holuphilix <Oluphilix@gmail.com>
2025-02-21 00:27:59 +0200

    changed message

--- source/app.js
+++ source/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/testNode', (_req, res) => {
-    res.status(200).send('Yes the testNode endpoint worked'); // Remove the comma
+    res.status(200).send('Yes the testNode endpoint worked and returned correct value '); // Remove the comma
 });
 
 module.exports = app;
