04b04433beed11227e91bdb64715b388754d5321
holuphilix <Oluphilix@gmail.com>
2025-02-22 18:11:43 +0200

    update for deployment to EC2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./app'); // Importing the Express app from app.js
+const app = require('./source/app'); // Updated to point to the "source" folder
 const port = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
 
 // Start the server and listen on the specified port
