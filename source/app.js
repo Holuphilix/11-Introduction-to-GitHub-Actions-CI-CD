caad97612e1d7719c48e3fe6d1c2bbf35ee44e65
holuphilix <Oluphilix@gmail.com>
2025-02-22 18:06:58 +0200

    added update  for  deployment to EC2

--- source/app.js
+++ source/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-// Define a simple route to test if the app is working
 app.get('/testNode', (_req, res) => {
-    res.status(200).send('Yes, the testNode endpoint worked'); // Fixed the response message
+    res.status(200).send('Yes the testNode endpoint worked'); // Remove the comma to match the test
 });
 
-module.exports = app; // Export the Express app for use in index.js
+module.exports = app;
