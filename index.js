964bde29d545d9b814bec57b0831179ef3cf5424
holuphilix <Oluphilix@gmail.com>
2025-02-22 18:35:51 +0200

    added update app.js  for  deployment to EC2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./source/app'); // Updated to point to the "source" folder
+const app = require('./source/app'); // Ensure this points to the correct file
 const port = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
 
 // Start the server and listen on the specified port
--- source/app.js
+++ source/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Root route for / (http://34.237.242.97:3000/)
+app.get('/', (_req, res) => {
+    res.send('Welcome to the Node.js app!'); // Custom message for the root route
+});
+
+// Test route (optional, for testing /testNode endpoint)
 app.get('/testNode', (_req, res) => {
-    res.status(200).send('Yes the testNode endpoint worked'); // Remove the comma to match the test
+    res.status(200).send('Yes the testNode endpoint worked');
 });
 
 module.exports = app;
