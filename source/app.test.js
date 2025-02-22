022a382b3fae553223b6b58f1e8bcdc3344af3b9
holuphilix <Oluphilix@gmail.com>
2025-02-22 18:02:56 +0200

    added update app.js  for  deployment to EC2

--- source/app.test.js
+++ source/app.test.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+const app = require('./app'); // Import app.js from the same directory
 const supertest = require('supertest');
 const request = supertest(app);
 
@@ -6,6 +6,6 @@ describe('/testNode endpoint', () => {
     it('should return a response', async () => {
         const response = await request.get('/testNode');
         expect(response.status).toBe(200); // Check the HTTP status code
-        expect(response.text).toBe('Yes the testNode endpoint worked'); // Match exact text
+        expect(response.text).toBe('Yes the testNode endpoint worked'); // Match the response text
     });
 });
